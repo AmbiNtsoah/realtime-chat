0851402c3d0055506f8a13298fe3c92209a7711d
AmbiNtsoah <ambinints04@gmail.com>
2025-05-22 10:32:33 +0300

    feat: Architecture placed with mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // Importer les modules, dépendences nécéssaires
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const server = express();
-const PORT = 6699;
+const environement = require('dotenv').config();
+
+const PORT = 9669;
+const url = process.env.MONGO_URL;
 
 server.get('/', (request, response) => {
     response.send('Hello Chat App');
 });
+
+
+mongoose.connect(url)
+.then(() => console.log('Database connected !'))
+.catch(() => console.log('Connexion Attempt Failed !'));
 
 server.listen(PORT, ()=> {
     console.log(`Server launched in http://localhost:${PORT}`);
